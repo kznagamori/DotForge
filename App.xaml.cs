b9523bf5887b9defee03d3a199a55e76af8c61e4
kznagamori <kazuyuki.nagamori@gmail.com>
2024-12-26 14:43:27 +0900

    エラー対策

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,5 +9,53 @@ namespace DotForge;
 /// </summary>
 public partial class App : Application
 {
+    private static Mutex? mutex = null;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        // 多重起動の防止
+        const string appName = "CodeReviewer"; // アプリケーション名
+        mutex = new Mutex(true, appName, out bool createdNew);
+
+        if (!createdNew)
+        {
+            // すでに起動している
+            MessageBox.Show("アプリケーションがすでに立ち上がっています。", "多重起動防止", MessageBoxButton.OK, MessageBoxImage.Warning);
+            Application.Current.Shutdown();
+            return;
+        }
+
+        // 未処理例外のキャッチ
+        AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+        {
+            Exception? exception = args.ExceptionObject as Exception;
+            if (exception != null)
+            {
+                MessageBox.Show(exception.ToString(), "未処理例外発生", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+        };
+
+        DispatcherUnhandledException += (sender, args) =>
+        {
+            MessageBox.Show(args.Exception.ToString(), "Dispatcher未処理例外発生", MessageBoxButton.OK, MessageBoxImage.Error);
+            args.Handled = true; // 例外処理済みとする
+            Application.Current.Shutdown();
+
+        };
+
+        base.OnStartup(e);
+
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        if (mutex != null)
+        {
+            mutex.ReleaseMutex();
+            mutex.Dispose();
+        }
+        base.OnExit(e);
+    }
 }
 
