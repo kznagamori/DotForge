4441a244acafaa6add5160b39b9aa301376ee62f
kznagamori <kazuyuki.nagamori@gmail.com>
2024-12-26 16:24:41 +0900

    データバインド

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,82 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace DotForge.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        // 行ごとの有効／無効を管理するプロパティ
+        [ObservableProperty]
+        private bool isTemplateRowEnabled = true;
+
+        [ObservableProperty]
+        private bool isProjectFileRowEnabled = true;
+
+        [ObservableProperty]
+        private bool isProjectNameRowEnabled = true;
+
+        [ObservableProperty]
+        private bool isClassNameRowEnabled = true;
+
+        [ObservableProperty]
+        private bool isOutputDirRowEnabled = true;
+
+        // ListBox 用データ
+        [ObservableProperty]
+        private ObservableCollection<string> templates = new()
+        {
+            "Template1", "Template2", "Template3"
+        };
+
+        // ListBox の選択アイテム
+        [ObservableProperty]
+        private string selectedTemplate = string.Empty;
+
+        // プロジェクトファイルパス
+        [ObservableProperty]
+        private string projectFilePath = string.Empty;
+
+        // プロジェクト名
+        [ObservableProperty]
+        private string projectName = string.Empty;
+
+        // クラス名
+        [ObservableProperty]
+        private string className = string.Empty;
+
+        // 出力先ディレクトリ
+        [ObservableProperty]
+        private string outputDirectory = string.Empty;
+
+        // コマンド: プロジェクトファイル選択
+        [RelayCommand]
+        private void OpenProjectFile()
+        {
+            // ここでファイルダイアログを開くなどの実装を行う想定
+            // 例: Microsoft.Win32.OpenFileDialog を使ってパスを取得
+            // ProjectFilePath = 取得したパス;
+        }
+
+        // コマンド: 出力先ディレクトリ選択
+        [RelayCommand]
+        private void OpenOutputDir()
+        {
+            // ここでフォルダブラウズダイアログを開くなどの実装を行う想定
+            // OutputDirectory = 取得したディレクトリ;
+        }
+
+        // コマンド: 出力処理
+        [RelayCommand]
+        private void GenerateOutput()
+        {
+            // 実際の出力処理を行う想定
+            // 例: ファイル生成・コンパイルなど
+        }
+
+        public MainViewModel()
+        {
+            // コンストラクタで初期値等を設定
+        }
+    }
+}
