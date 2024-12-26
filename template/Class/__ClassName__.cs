0eb31fe114ab6430237d9be8bbde6e39888650e9
kznagamori <kazuyuki.nagamori@gmail.com>
2024-12-26 17:23:42 +0900

    テンプレートディレクトリを登録

---
+++ template/Class/__ClassName__.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace __ProjectName__;
+public partial class __ClassName__
+{
+    public __ClassName__()
+    {
+        InitializeComponent();
+    }
+}
+
