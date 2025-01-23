af4fd067b61e10647f27eb1f917ee68cd0730264
kznagamori <kazuyuki.nagamori@gmail.com>
2025-01-23 20:45:53 +0900

    ファイル・ディレクトリ構成を変更

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DotForge;
+namespace DotForge.Views;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
